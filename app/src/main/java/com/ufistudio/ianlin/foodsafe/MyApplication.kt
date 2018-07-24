8775fe51a15c265fba6789d23892ff20d3ac0ad4
ian lin <ian.lin@ufispace.com>
2018-07-24 21:48:08 +0800

    [modify] add fabric

--- app/src/main/java/com/ufistudio/ianlin/foodsafe/MyApplication.kt
+++ app/src/main/java/com/ufistudio/ianlin/foodsafe/MyApplication.kt
@@ -1,11 +1,18 @@
 package com.ufistudio.ianlin.foodsafe
 
 import android.app.Application
+import com.crashlytics.android.Crashlytics
+import io.fabric.sdk.android.Fabric
 
 class MyApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
         AppInjector.init(this)
+        val fabric = Fabric.Builder(this)
+                .kits(Crashlytics())
+                .debuggable(true)
+                .build()
+        Fabric.with(fabric)
     }
 }
