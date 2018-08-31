312ec9bc0931233d14efc7a97bdfc4b730184d71
ian lin <ian.lin@ufispace.com>
2018-08-31 17:50:47 +0800

    [modify] remove unused import

--- app/src/main/java/com/ufistudio/ianlin/foodsafe/pages/main/information/InformationViewModel.kt
+++ app/src/main/java/com/ufistudio/ianlin/foodsafe/pages/main/information/InformationViewModel.kt
@@ -5,8 +5,6 @@ import android.arch.lifecycle.MutableLiveData
 import com.ufistudio.ianlin.foodsafe.repository.viewModel.BaseViewModel
 import com.ufistudio.ianlin.foodsafe.repository.Repository
 import com.ufistudio.ianlin.foodsafe.repository.data.Category
-import com.ufistudio.ianlin.foodsafe.repository.provider.preferences.SharedPreferencesProvider
-import com.ufistudio.ianlin.foodsafe.repository.provider.resource.ResourceProvider
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 
